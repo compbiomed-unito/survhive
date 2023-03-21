c959c09ad85f0002e8ffd257509b7d798a2d7e52
Ivan Rossi <ivan.rossi@unito.it>
2023-03-21 16:40:57 +0100

    Introdotto pytest   - primo test stupido OK   - test di utilizzo di black come code formatter

---
+++ tests/test_sksurv_base.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy
+
+# ## Mini example for scikit-survival estimators
+
+
+def load_toy_data():
+    "Load standard dataset for testing"
+    import sksurv.datasets
+    from sklearn.preprocessing import OneHotEncoder
+
+    X, y = sksurv.datasets.load_breast_cancer()
+    X = numpy.concatenate(
+        [
+            X.select_dtypes("float"),
+            OneHotEncoder(sparse_output=False).fit_transform(
+                X.select_dtypes("category")
+            ),
+        ],
+        axis=1,
+    )
+    return X, y
+
+
+def test_sksurv_data_is_loading():
+    X, y = load_toy_data()
+    assert X.shape == (198, 84)
+
+
+### ------------
+
+# # Fit penalized Cox model (from scikit-survival)
+
+# from sksurv.linear_model import CoxnetSurvivalAnalysis
+# from sksurv.metrics import concordance_index_censored
+
+# model = CoxnetSurvivalAnalysis()
+# model.fit(X, y)
+# pred = model.predict(X)
+
+
+# # Standard functions from scikit-learn can be used with scikit-survival models
+
+# # In[6]:
+
+
+# from sklearn.model_selection import cross_val_score
+
+
+# # In[7]:
+
+
+# cross_val_score(CoxnetSurvivalAnalysis(), X, y)
+
+
+# ## Scikit-learn compatibility
+# Scikit-learn has a checker for estimators to see if they conform to their specification.
+
+# # In[8]:
+
+# from sklearn.utils.estimator_checks import check_estimator
+
+
+# # Scikit-survival models do not necessarily pass ;-)
+
+# # In[9]:
+
+
+# try:
+#     check_estimator(CoxnetSurvivalAnalysis())
+# except Exception as e:
+#     print(e.__class__.__name__)
+#     print(e)
