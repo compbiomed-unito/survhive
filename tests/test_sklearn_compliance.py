bf29feafec184b64d8ce33c782a0d0e735d88bf8
Ivan Rossi <ivan.rossi@unito.it>
2023-03-22 18:18:21 +0100

    tests sugli adaptes di base: BaseEstimator, SurvivalEstimator

---
+++ tests/test_sklearn_compliance.py
@@ -0,0 +1,27 @@
+# ## Scikit-learn compatibility
+
+# Scikit-learn has a checker for estimators to see if they conform to their
+# specification.
+#
+# Scikit-survival models do not necessarily pass ;-)
+
+from sklearn.utils.estimator_checks import check_estimator
+from survwrap import *
+
+
+def sklearn_compliance(TestedEstimator):
+    "Check esitmator for scikit-learn compliance"
+    try:
+        check_estimator(TestedEstimator())
+    except Exception as e:
+        print(e.__class__.__name__)
+        print(e)
+
+#_estimators = [ BaseEstimator ]
+
+def test_base_estimator_compliance():
+    sklearn_compliance(BaseEstimator)
+
+def test_survival_estimator_compliance():
+    sklearn_compliance(SurvivalEstimator)
+
