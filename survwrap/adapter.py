9c521992dbaf45223c880b821ff621b3b6c4c778
Ivan Rossi <ivan.rossi@unito.it>
2023-06-12 18:44:58 +0200

    dev: primo wrapper che compila

--- survwrap/adapter.py
+++ survwrap/adapter.py
@@ -24,3 +24,42 @@ class SurvivalEstimator(BaseEstimator):
     def predict(self, X):
         X = check_array(X)
         return np.full(shape=X.shape[0], fill_value=self.param1)
+
+
+@dataclass
+class CoxNet(SurvivalEstimator):
+    """
+    Adapter for the CoxNet method from scikit-survival
+    """
+    from sksurv.linear_model import CoxnetSurvivalAnalysis
+
+    package = "scikit-survival"
+    model_ = CoxnetSurvivalAnalysis()
+
+    # init
+    l1_ratio: float = 0.5
+    verbose: bool = False
+    fit_baseline_model: bool = False
+    score: float = 0.0
+
+    def fit(self, X, y):
+
+        X, y = check_X_y(X, y)
+        self.model_.set_params(
+            l1_ratio=self.l1_ratio,
+            verbose=self.verbose,
+            fit_baseline_model=self.fit_baseline_model
+            )
+        self.model_ = self.model_.fit(X, y)
+        return self
+
+    def predict(self, X):
+        X = check_array(X)
+        return self.model_.predict(X)
+
+    def score(self, X, y):
+        X = check_array(X)
+        return self.model_.score(X, y)
+
+
+
