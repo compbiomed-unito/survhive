0557848b3d4e96c803b830062577d996279f6a76
Ivan Rossi <ivan.rossi@unito.it>
2023-07-03 12:39:46 +0200

    importato il codice di Giovanni, passa il test di compliance di sklearn Limitato a fit e predict

--- survwrap/__init__.py
+++ survwrap/__init__.py
@@ -1,3 +1,4 @@
 from .adapter import *
 from .sksurv_adapters import *
+from .pycox_adapters import *
 from .util import *
--- tests/test_sklearn_compliance.py
+++ tests/test_sklearn_compliance.py
@@ -2,8 +2,6 @@
 
 # Scikit-learn has a checker for estimators to see if they conform to their
 # specification.
-#
-# Scikit-survival models do not necessarily pass ;-)
 
 from sklearn.utils.estimator_checks import check_estimator
 from survwrap import *
@@ -22,8 +20,13 @@ def sklearn_compliance(TestedEstimator):
 def test_survival_estimator_compliance():
     sklearn_compliance(SurvivalEstimator)
 
-# CoxNet
+# CoxNet from scikit-survival
 
 def test_wrapped_coxnet_compliance():
     sklearn_compliance(CoxNet)
+
+# DeepHitSingle from pycox
+
+def test_wrapped_deephitsingle_compliance():
+    sklearn_compliance(DeepHitSingle)
 
