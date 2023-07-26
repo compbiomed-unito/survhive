27d75b08df28cfd099de665608d62881b66259fb
Giovanni Birolo <giovanni.birolo@unito.it>
2023-07-26 07:34:12 +0000

    added tentative new version of data loader

--- survwrap/util.py
+++ survwrap/util.py
@@ -1,12 +1,13 @@
 import numpy
+import sksurv.datasets
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
 
-
-def load_test_data():
+def load_test_data(dataset='breast_cancer'):
     "Load standard dataset for testing"
-    import sksurv.datasets
-    from sklearn.preprocessing import OneHotEncoder
 
-    X, y = sksurv.datasets.load_breast_cancer()
+    #X, y = sksurv.datasets.load_breast_cancer()
+    X, y = getattr(sksurv.datasets, 'load_' + dataset)()
     X = numpy.concatenate(
         [
             X.select_dtypes("float"),
@@ -16,6 +17,8 @@ def load_test_data():
         ],
         axis=1,
     )
+    X = SimpleImputer(strategy='median').fit_transform(X)
+
     return X, y
 
 
