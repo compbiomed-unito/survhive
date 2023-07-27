c7e9179d603d1d89a86348b4fa50a5844dddab21
Ivan Rossi <ivan.rossi@unito.it>
2023-07-27 12:56:30 +0200

    dev: added a new "dataset"type and some auxiliary functions to readily get some benchmark datasets

--- survwrap/util.py
+++ survwrap/util.py
@@ -2,7 +2,7 @@ import numpy
 
 
 def load_test_data():
-    "Load standard dataset for testing"
+    "Load standard breast-cancer dataset for testing"
     import sksurv.datasets
     from sklearn.preprocessing import OneHotEncoder
 
@@ -27,3 +27,45 @@ def get_indicator(y):
 def get_time(y):
     "Get the time of the event"
     return y[y.dtype.names[1]]
+
+
+## datasets
+
+import pandas as pd
+from sksurv.datasets import get_x_y
+from dataclasses import dataclass, field
+
+
+_dataset_path = "Datasets/"
+_available_datasets = (
+    "flchain",
+    "gbsg2",
+    "metabric",
+    "support",
+)
+
+
+def list_available():
+    "list the available benchmark datasets"
+    return _available_datasets
+
+
+def get_data(set_name):
+    "Load one of the available benchmark datasets as a dataset object"
+    if set_name not in _available_datasets:
+        raise NameError("Dataset not available.")
+    return dataset(
+        name=set_name,
+        dataframe=pd.read_csv(_dataset_path + set_name + ".csv", index_col=0),
+    )
+
+
+@dataclass
+class dataset:
+    "a dataset container"
+    name: str
+    dataframe: field(default_factory=pd.DataFrame)
+
+    def get_X_y(self):
+        "return dataset in scikit-survival format"
+        return get_x_y(self.dataframe, attr_labels=["event", "time"], pos_label=1)
