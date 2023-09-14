adb292cc5f7a4c9744ffd0fae33960372282fd8e
Ivan Rossi <ivan.rossi@unito.it>
2023-09-14 18:06:40 +0200

    tested optimization._guess_tries

--- tests/test_optimization.py
+++ tests/test_optimization.py
@@ -11,3 +11,15 @@ def test_topology_grid():
     _topologies = [[3], [4], [5], [7], [9], [3, 3], [4, 4], [5, 5], [7, 7], [9, 9]]
 
     assert tosa.generate_topology_grid(9, max_layers=2) == _topologies
+
+
+def test_guess_tries():
+    "test optimization._guess_tries function"
+    dhs_guess = tosa.optimization._guess_tries(
+        tosa.DeepHitSingle.get_parameter_grid(max_width=X.shape[1]), fraction=0.2
+    )
+    assert dhs_guess == 16
+    dsm = tosa.optimization._guess_tries(
+        tosa.DeepSurvivalMachines.get_parameter_grid(max_width=X.shape[1]),
+    )
+    assert dsm == 24
