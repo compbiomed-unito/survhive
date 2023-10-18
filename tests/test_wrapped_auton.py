0c4166ce004766f255d4284cea2844a9ed1af63b
Ivan Rossi <ivan.rossi@unito.it>
2023-10-18 17:30:49 +0200

    dev: predict_survival tests for auton dsm

--- tests/test_wrapped_auton.py
+++ tests/test_wrapped_auton.py
@@ -1,16 +1,17 @@
 import pytest
-import survwrap as tosa
+import survwrap
 from common import basic_test
 
 # init test data
 dsm_test = basic_test()
-dsm_test.model = tosa.DeepSurvivalMachines(
+dsm_test.model = survwrap.DeepSurvivalMachines(
     rng_seed=2307, batch_size=10, max_epochs=20, layer_sizes=[10, 10, 10]
 )
-dsm_test.X, dsm_test.y = tosa.load_test_data()
+dsm_test.X, dsm_test.y = survwrap.load_test_data()
 dsm_test.exp_score = 0.62
 dsm_test.exp_cv_mean = 0.61
 dsm_test.exp_cv_std = 0.04
+dsm_test.exp_survival = [[0.93, 0.91, 0.84]]
 dsm_test.rounding = 2
 
 
